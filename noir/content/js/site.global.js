b7e99bfa1023afc635df56e6367757f464fe8761
iMacmini <imacmini@iMacminideMac-mini.local>
2018-12-25 23:52:47 +0800

    Update site.global.js

--- noir/content/js/site.global.js
+++ noir/content/js/site.global.js
@@ -198,7 +198,7 @@ var SITE = SITE || {};
 			// init item opacity
 			// item.css('opacity', init_item_opa);
 			item.each(function() {
-				var delayTime = Math.floor(Math.random() * 500) + 300,
+				var delayTime = Math.floor(Math.random() * 1000) + 200,
 					obj = this;				
 				setTimeout(function() {
 				    // fade out item				   
