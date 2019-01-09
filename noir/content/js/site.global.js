644098699f208990ce7ec22e25db3673d49bb941
iMacmini <imacmini@iMacminideMac-mini.local>
2019-01-09 22:26:44 +0800

    Update site.global.js

--- noir/content/js/site.global.js
+++ noir/content/js/site.global.js
@@ -277,7 +277,7 @@ var SITE = SITE || {};
 					var item = data[i];
 
 					var contStr = '<li class="flipWorld"><div id="project_' + item.id + '" class="flipCon works_item_wrapper">' +
-					'<div class="popup"><div class="works_item" data-rid="' + item.rid + '" style="cursor: pointer;"><img src="' + item.thumbImg + '" alt="' + id + '"></div>' + '</div>' +
+					'<div class="popup"><div class="works_item" data-rid="' + item.rid + '" style="cursor: pointer;"><img src="' + item.thumbImg + '" alt="' + item.id + '"></div>' + '</div>' +
 					'<div class="detail">' +
 					'<div class="detail_wrapper">' +
 					'<h4 class="detail_title">' + item.title + '</h4>' + 
