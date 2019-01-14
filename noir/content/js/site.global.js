d7a5a3b579fe4a16b0f51150b67efbb59d90f3eb
iMacmini <imacmini@iMacminideMac-mini.local>
2019-01-14 11:12:34 +0800

    Update site.global.js

--- noir/content/js/site.global.js
+++ noir/content/js/site.global.js
@@ -318,8 +318,8 @@ var SITE = SITE || {};
 					// $.getJSON('content/ajax/gallery.json', function (data) {
 						console.log(data);
 						console.log(data[0]);
-						// console.log(data[0][0]);
-						var arr = data[0]['popupImg'+index],
+						console.log(data[0][index]);
+						var arr = data[0][index]['popupImg'+index],
                             len = arr.length;
 						initPhotoSwipe(arr,len);
 					// });
