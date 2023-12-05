6b963fca1e473214b85caf81b1672726cec88683
c-sean <45257662+c-sean@users.noreply.github.com>
2023-12-05 23:21:18 +0800

    1205

--- resume/script.js
+++ resume/script.js
@@ -1,45 +1,4 @@
-// const container = document.getElementById("swiper");
-// container.addEventListener("wheel", mousewheel, false);
 
-// const multiplier = 2;
-// const center = 2500;
-// const interesting = 500;
-
-// function mousewheel(event) {
-//   event.preventDefault();
-//   let dx = event.deltaX * multiplier;
-//   if (Math.abs(container.scrollTop - center) < interesting){
-//     dx /= 25;
-//   }
-//   container.scrollLeft += dx;
-  
-// }
-
-// var $slider = $('.slider'),
-//     length = [];
-    // const slider = $(".slider-item");
-// slider
-//     .slick({
-//         // slidesToShow: 5,
-//         slidesToScroll: 1,
-//         dots: false,
-//         centerMode: true,
-//         focusOnSelect: true,
-//         // dots: true,
-//         /* vertical: true, */
-//         verticalSwiping: true
-
-//     });
-
-// slider.on('wheel', (function(e) {
-//     e.preventDefault();
-
-//     if (e.originalEvent.deltaY < 0) {
-//         $(this).slick('slickPrev');
-//     } else {
-//         $(this).slick('slickNext');
-//     }
-// }));
 var swiper = new Swiper(".mySwiper", {
       // direction: "vertical",
    forceToAxis: true,
@@ -56,48 +15,6 @@ var swiper = new Swiper(".mySwiper", {
     el: '.swiper-scrollbar',
     draggable: true
   }
-    //   breakpoints: {
-    // // when window width is >= 320px
-    //     300: {
-    //       // slidesPerView: 2,
-    //       spaceBetween: 0
-    //     },
-    //     // when window width is >= 480px
-    //     485: {
-    //       // slidesPerView: 3,
-    //       spaceBetween: 0
-    //     }
-    //     // when window width is >= 640px
-    //     // 640: {
-    //     //   slidesPerView: 4,
-    //     //   spaceBetween: 40
-    //     // }
-    //   },
-      // on:{
-      //     slideChangeTransitionEnd: function () {
-      //     if(swiper.activeIndex == 0) {
-      //       transNum(2023);
-      //     }
-      //     else  { 
-      //       resetNum(1980);
-      //     }
-      //   }
-      //    slideChange: function (){
-      //       if (swiper.activeIndex != 7) { 
-      //       progressInit();
-      //     }
-
-      //    },
-      //    scrollbarDragEnd: function(){
-      //       console.log('done');
-      //       if (swiper.activeIndex == 7) { 
-      //         progressAnimate();
-      //       }
-      //       else  { 
-      //         progressInit();
-      //       }
-      //    }
-      // }
     });
 swiper.on('slideChangeTransitionEnd', anime);
 swiper.on('scrollbarDragEnd', anime);
@@ -121,7 +38,6 @@ var myElement = document.getElementById('swiper');
 var hammer = new Hammer(myElement);
 hammer.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 hammer.on('swipeup', function() {
-  // console.log('up');
   swiper.slideNext();
 });
 hammer.on('swipedown', function() {
@@ -136,8 +52,8 @@ $('.slider').each(function(i,e) {
       $(this).children().each(function() {
           length[i] += $(this).outerHeight(true);
       })
-      // $('<style>').html('@keyframes loop' + i + '{from {transform:translate3d(0,0,0);}to{transform:translate3d(0,' + -length[i] + 'px,0);}}').appendTo('head');
-      // $('<style>').html('@keyframes loop' + i + '{from {top:0;}to{top:' + -length[i] + 'px;}}').appendTo('head');
+      $('<style>').html('@keyframes loop' + i + '{from {transform:translate3d(0,0,0);}to{transform:translate3d(0,' + -length[i] + 'px,0);}}').appendTo('head');
+      $('<style>').html('@keyframes loop' + i + '{from {top:0;}to{top:' + -length[i] + 'px;}}').appendTo('head');
 
       $(this).children().clone().addClass('slider_clone').removeClass('slider_origin').appendTo($(this)).parent().css({
         'animation': 'loop' + i + ' linear '+ length[i]/80 +'s  infinite'
@@ -145,40 +61,9 @@ $('.slider').each(function(i,e) {
       $(this).children().clone().addClass('slider_clone').removeClass('slider_origin').appendTo($(this));
 });
 })
-  //   $slider.on({'mouseleave': function() {
-  //    $(this).css({'animation-play-state':'running'});
-    
-  //   }, 'mouseenter': function(){
-  //    $(this).css({'animation-play-state':'paused'});
-  //   }
-  // });
-  //   $('.swiper').on({'mousedown': function(){
-  //       $(this).css({'cursor':'url(img/closedhand.cur),move'});
-  //     }, 'mouseup ': function(){
-  //       $(this).css({'cursor':'url(img/openhand.cur),move'});
-  //     }
-  // });
 
-// $()
-// var animation = new Swiper(".item", {
-//   direction: "vertical",
-//   loop: true,
-//         speed: 2000,
-//         // slidesPerView: 'auto',    
-//         autoplay: {
-//             delay: 0,
-//             pauseOnMouseEnter: true,        // stop autoplay when hovering
-//             disableOnInteraction: false,    // restart autoplay when hover is removed
-//             // reverseDirection: true,         // reverse the autoplay direction
-//         },
-// });
 
-// $('.counter').counter({
-// 	// autoStart: true, 
-// 	// duration: 5000,
-// 	// countFrom: 2005,              // start counting at this number, default: 0
-//   	countTo: 2023,  
-// });
+
 var el = document.querySelector('.odometer');
  
 var od = new Odometer({
@@ -200,22 +85,12 @@ const numbers = [1, 9, 8, 0];
 
 function resetNum(num) {
   $('.odometer-value').each(function(i,val){
-    // console.log($(val).html());
     $(val).html(numbers[i]);
   })
   od.value=num;
 }
 
 transNum(2023);
-// window.odometerOptions = {
-//   selector: '.odometer', 
-//   format: 'd'
-// }
-
-// swiper.on('orientationchange', function () {
-//   console.log('slide changed');
-
-// });
 
 
   var lineHtml = new ProgressBar.Line('.html', {easing: 'easeInOut', trailWidth: .5, strokeWidth: 1.5, color: '#e54c20'});
